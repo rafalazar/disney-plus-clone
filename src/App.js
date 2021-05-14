539f4266ca7bcbe3ced7e06bc36e104ee9d791c9
Jordan <jcr.salazar@outlook.com>
2021-05-14 16:37:54 -0500

    Login Screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Router>
+
+        <Header />
 
         <Switch>
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -8,6 +8,7 @@ const Login = () => {
                     <CTALogoOne src='/images/cta-logo-one.svg' alt='logo one' />
                     <SignUp>GET ALL THERE</SignUp>
                     <Description>Get Premier Access to Raya and the Last Dragon for an additional fee with a Disney+ subscription. As of 03/26/21, the price of Disney+ and The Disney Bundle will increase by $1.</Description>
+                    <CTALogoTwo src='/images/cta-logo-two.png' alt='logo two' />
                 </CTA>
                 <BgImage />
             </Content>
@@ -55,11 +56,12 @@ const CTA = styled.div`
     display: flex;
     flex-direction: column;
     width: 100%;
+    align-items: center;
 `;
 
 const CTALogoOne = styled.img`
     margin-bottom: 12px;
-    max-width: 650px;
+    max-width: 600px;
     min-height: 1px;
     display: block;
     width: 100%;
@@ -80,6 +82,22 @@ const SignUp = styled.a`
     &:hover {
         background-color: #0483ee;
     }
+`;
+
+const Description = styled.p`
+    color: hsla(0, 0%, 95.3%, 1);
+    font-size: 11px;
+    margin: 0 0 24px;
+    line-height: 1.5;
+    letter-spacing: 1.5px;
+`;
+
+const CTALogoTwo = styled.img`
+    max-width: 600px;
+    margin-bottom: 20px;
+    display: inline-block;
+    vertical-align: bottom;
+    width: 100%;
 `;
 
 export default Login;
