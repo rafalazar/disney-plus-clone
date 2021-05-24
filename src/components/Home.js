7be6b78c0d365a0ce023bd278d63f4223367d1b7
Jordan <jcr.salazar@outlook.com>
2021-05-24 13:00:30 -0500

    firebase data added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,65 @@
+import {useEffect} from 'react';
 import styled from 'styled-components';
 import ImgSlider from './ImgSlider';
+import NewDisney from './NewDisney';
+import Originals from './Originals';
+import Recommends from './Recommends';
+import Trending from './Trending';
 import Viewers from './Viewers';
+import { useDispatch, useSelector } from 'react-redux';
+import db from '../firebase';
+import { setMovies } from '../features/movie/movieSlice';
+import { selectUserName } from '../features/user/userSlice';
 
 const Home = () => {
+
+    const dispatch = useDispatch();
+    const userName = useSelector(selectUserName);
+    let recommends = [];
+    let newDisneys = [];
+    let originals = [];
+    let trendings = [];
+    
+    useEffect(() => {
+        db.collection('movies').onSnapshot((snapshot) => {
+            snapshot.docs.map((doc) => {
+                switch(doc.data().type) {
+                    case 'recommend':
+                        recommends = [...recommends, {id: doc.id, ...doc.data()}];
+                        break;
+
+                    case 'new':
+                        newDisneys = [...newDisneys, {id: doc.id, ...doc.data()}];
+                        break;
+
+                    case 'original':
+                        originals = [...originals, {id: doc.id, ...doc.data()}];
+                        break;
+
+                    case 'trending':
+                        trendings = [...trendings, {id: doc.id, ...doc.data()}];
+                        break;
+                }
+            });
+
+            dispatch(setMovies({
+                recommend: recommends,
+                newDisney: newDisneys,
+                original: originals,
+                trending: trendings
+            }));
+
+        });
+    }, [userName]);
 
     return (
         <Container>
             <ImgSlider />
             <Viewers />
+            <Recommends />
+            <NewDisney />
+            <Originals />
+            <Trending />
         </Container>
     );
 }
