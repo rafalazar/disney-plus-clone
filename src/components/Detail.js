e0719ce3273422196088e4d1bcc525ec57812cf1
Jordan <jcr.salazar@outlook.com>
2021-05-26 11:26:07 -0500

    Detail of movie added

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,14 +1,33 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import db from '../firebase';
 
 const Detail = () => {
+
+    const { id } = useParams();
+    const [detailData, setDetailData] = useState({});
+
+    useEffect(() => {
+        db.collection('movies').doc(id).get().then((doc) => {
+            if(doc.exists) {
+                setDetailData(doc.data())
+            } else {
+                console.log('No data in firebase');
+            }
+        }).catch((error) => {
+            console.log('Error getting document: ', error);
+        })
+    }, [id]);
+
     return (
         <Container>
             <Background>
-                <img src="https://i0.wp.com/virtualgamer.es/wp-content/uploads/2021/05/eternals-marvel-disney-trailer-portada.jpg?fit=1892%2C1065&ssl=1" alt="wallpaper" />
+                <img src={detailData.backgroundImg} alt={detailData.title} />
             </Background>
 
             <ImgTitle>
-                <img src="https://terrigen-cdn-dev.marvel.com/content/prod/1x/eternals_lob_log_def_03.png" alt="logo" />
+                <img src={detailData.titleImg} alt={detailData.title} />
             </ImgTitle>
 
             <ContentMeta>
@@ -22,7 +41,27 @@ const Detail = () => {
                         <img src="https://i.ibb.co/f9gyJQf/play-icon-white.png" alt="trailer-icon-white" />
                         <span>Trailer</span>
                     </Trailer>
+
+                    <AddList>
+                        <span />
+                        <span />
+                    </AddList>
+
+                    <GroupWatch>
+                        <div>
+                            <img src="https://i.ibb.co/vQQ29M7/group-icon.png" alt="group-watch-icon" />
+                        </div>
+                    </GroupWatch>
                 </Controls>
+
+                <SubTitle>
+                    {detailData.subTitle}
+                </SubTitle>
+
+                <Description>
+                    {detailData.description}
+                </Description>
+
             </ContentMeta>
         </Container>
     );
@@ -56,12 +95,12 @@ const Background = styled.div`
 `;
 
 const ImgTitle = styled.div`
+    -webkit-box-pack: start;
     display: flex;
     align-items: flex-end;
-    -webkit-box-pack: start;
     justify-content: flex-start;
     margin: 0px auto;
-    height: 30vh;
+    height: 23vw;
     min-height: 170px;
     padding-bottom: 24px;
     width: 100%;
@@ -126,6 +165,79 @@ const Trailer = styled(Player)`
     background: rgba(0, 0, 0, 0.3);
     border: 1px solid rgb(249, 249, 249);
     color: rgb(249, 249, 249);
+`;
+
+const AddList = styled.div`
+    margin-right: 16px;
+    height: 44px;
+    width: 44px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(0, 0, 0, 0.6);
+    border-radius: 50%;
+    border: 2px solid white;
+    cursor: pointer;
+
+    span {
+        background-color: rgb(249, 249, 249);
+        display: inline-block;
+
+        &:first-child {
+            height: 2px;
+            transform: translate(1px, 0px) rotate(0deg);
+            width: 16px;
+        }
+
+        &:nth-child(2) {
+            height: 16px;
+            transform: translate(-8px) rotate(0deg);
+            width: 2px;
+        }
+    }
+`;
+
+const GroupWatch = styled.div`
+    height: 44px;
+    width: 44px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    background: white;
+
+    div {
+        height: 40px;
+        width: 40px;
+        background: rgb(0, 0, 0);
+        border-radius: 50%;
+
+        img {
+            width: 100%;
+        }
+    }
+`;
+
+const SubTitle = styled.div`
+    color: rgb(249, 249, 249);
+    font-size: 15px;
+    min-height: 20px;
+
+    @media (max-width: 768px) {
+        font-size: 12px;
+    }
+`;
+
+const Description = styled.div`
+    line-height: 1.4;
+    font-size: 20px;
+    padding: 16px 0px;
+    color: rgb(249, 249, 249);
+
+    @media (max-width: 768px) {
+        font-size: 14px;   
+    }
 `;
 
 export default Detail;
